45717db7bc61407016a3aaa85ac1f5882f6d105d
Deni MMalyono <maximalyono@gmail.com>
2019-12-29 00:32:51 +0700

    sampai di asg

--- main.tf
+++ main.tf
@@ -2,30 +2,55 @@ provider "aws" {
 	region = "ap-southeast-1"
 }
 
-resource "aws_instance" "instance-ku" {
-	ami = "ami-061eb2b23f9f8839c"
+resource "aws_launch_configuration" "example" {
+	image_id = "ami-061eb2b23f9f8839c"
 	instance_type = "t2.micro"
-	vpc_security_group_ids = ["${aws_security_group.sgroup.id}"]
+	security_groups = [aws_security_group.sec-group.id]
 
 	user_data = <<-EOF
-					#!/bin/bash	
-						echo "Hello, world" > index.html
-						nohup busybox httpd -f -p 8080 &
+					#!/bin/bash
+						echo "Hello bung" > index.html
+						nohup busybox httpd -f -p ${var.server_port} & 
 						EOF
 
-	tags = {
-		Name = "yiuhu-yiuhu"
+	lifecycle {
+		create_before_destroy = true
 	}
 }
 
-resource "aws_security_group" "sgroup" {
+resource "aws_autoscaling_group" "sample" {
+	launch_configuration = aws_launch_configuration.example.name
+	vpc_zone_identifier = data.aws_subnet_ids.default.ids
 
-	name = "terraform-instance-ku"
+	min_size = 2
+	max_size = 10
 
- 	ingress {
-		from_port = 8080
-		to_port = 8080
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
+	tag {
+		key = "Name"
+		value = "asg-terraform"
+		propagate_at_launch = true
 	}
+}
+
+resource "aws_security_group" "sec-group" {
+	name = "terraformer"
+
+	ingress {
+		from_port = var.server_port
+		to_port = var.server_port
+		cidr_blocks = ["0.0.0.0/0"]
+		protocol = "tcp"
+	}
+}
+
+variable "server_port" {
+	default = 8080
+}
+
+data "aws_vpc" "default" {
+	default = true
+}
+
+data "aws_subnet_ids" "default" {
+	vpc_id = data.aws_vpc.default.id
 }
